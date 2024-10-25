0cfe919d068a455ca1898d0715eccb794f0d35ab
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2024-10-25 17:44:04 +0700

    update Splash, Login , Main

--- GUI/Login.cs
+++ GUI/Login.cs
@@ -15,7 +15,26 @@ namespace GUI
         public Login()
         {
             InitializeComponent();
+            SetupForm(); // Gọi phương thức SetupForm để thiết lập form
         }
 
+        private void SetupForm()
+        {
+            // Đặt kích thước form tiêu chuẩn
+            this.Size = new Size(1024, 768);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // Form không thể thay đổi kích thước
+            this.StartPosition = FormStartPosition.CenterScreen; // Đặt form xuất hiện giữa màn hình
+
+            string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Images\sgu.jpg");
+            if (File.Exists(imagePath))
+            {
+                this.BackgroundImage = Image.FromFile(imagePath);
+                this.BackgroundImageLayout = ImageLayout.Stretch; // Kéo dãn hình nền để vừa với form
+            }
+            else
+            {
+                MessageBox.Show("Image not found! " + imagePath);
+            }
+        }
     }
 }
