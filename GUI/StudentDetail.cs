8b6fd1651a2e4d81b0dabc9828259e60c4e752f0
QuanHuynh123 <huynhminhquan07072002@gmail.com>
2024-12-12 11:18:37 +0700

    move transcritp , timetable into studentDetail

--- GUI/StudentDetail.cs
+++ GUI/StudentDetail.cs
@@ -89,13 +89,13 @@ namespace GUI
         private void openTranscript()
         {
 
-            //Transcript transcript = 
-                new Transcript(studentID, student.TotalCreditsEarned, student.Gpa).Show();
-            //{
-            //    TopLevel = false, 
-            //    FormBorderStyle = FormBorderStyle.None,
-            //    Dock = DockStyle.Fill 
-            //};
+            Transcript transcript =
+                new Transcript(studentID, student.TotalCreditsEarned, student.Gpa) //.Show();
+            {
+                TopLevel = false, 
+                FormBorderStyle = FormBorderStyle.None,
+                Dock = DockStyle.Fill
+            };
 
             //List<Control> oldControls = new List<Control>();
             //foreach (Control control in tableLayoutPanel1.Controls)
@@ -103,52 +103,57 @@ namespace GUI
             //    oldControls.Add(control);
             //}
 
-            //transcript.Show();
-            ////transcript.BringToFront();
-            //tableLayoutPanel1.Controls.Clear();
-            //tableLayoutPanel1.ColumnCount = 1;
-            //tableLayoutPanel1.RowCount = 1; 
-            //tableLayoutPanel1.Controls.Add(transcript, 0, 0);
+            transcript.Show();
+            //transcript.BringToFront();
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.Controls.Add(transcript, 0, 0);
 
-            //transcript.FormClosed += (sender, e) =>
-            //{
-            //    // Khôi phục các control cũ sau khi đóng
-            //    tableLayoutPanel1.Controls.Clear();
-            //    tableLayoutPanel1.RowCount = 1;
-            //    tableLayoutPanel1.ColumnCount = 2;
-            //    foreach (var control in oldControls)
-            //    {
-            //        tableLayoutPanel1.Controls.Add(control);
-            //    }
-               
-            //    init(this.studentID);
-            //    oldControls.Clear();
-            //};
+            transcript.FormClosed += (sender, e) =>
+            {
+                new StudentDetail(studentID).Show();
+                this.Close();
+                //// Khôi phục các control cũ sau khi đóng
+                //tableLayoutPanel1.Controls.Clear();
+                //tableLayoutPanel1.RowCount = 1;
+                //tableLayoutPanel1.ColumnCount = 2;
+                //foreach (var control in oldControls)
+                //{
+                //    tableLayoutPanel1.Controls.Add(control);
+                //}
+
+                //init(this.studentID);
+                //oldControls.Clear();
+            };
 
         }
 
 
         private void openTimetable()
         {
-            //Timetable timetable =  
-                new Timetable(studentID).Show();
-            //{
-            //    TopLevel = false,
-            //    FormBorderStyle = FormBorderStyle.None, 
-            //    Dock = DockStyle.Fill 
-            //};
+            Timetable timetable =
+                new Timetable(studentID) //.Show();
+            {
+                TopLevel = false,
+                FormBorderStyle = FormBorderStyle.None, 
+                Dock = DockStyle.Fill
+            };
 
             //timetable.BringToFront();
-            //tableLayoutPanel1.Controls.Add(timetable, 0, 0);
-            //tableLayoutPanel1.SetColumnSpan(timetable, 2);
+            timetable.Show();
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.Controls.Add(timetable, 0, 0);
 
-            //timetable.FormClosed += (sender, e) =>
-            //{
-            //    tableLayoutPanel1.Controls.Remove(timetable);
-            //    init(this.studentID);
-            //};
-
-            //timetable.Show();
+            timetable.FormClosed += (sender, e) =>
+            {
+                new StudentDetail(studentID).Show();
+                this.Close();
+                //tableLayoutPanel1.Controls.Remove(timetable);
+                //init(this.studentID);
+            };
         }
     }
 }
